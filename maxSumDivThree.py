42c8e2a71be608328d32e22e780cc3948c8061fb
valli <vallibaddireddy@gmail.com>
2025-10-29 17:48:37 +0530

    leetcode

---
+++ maxSumDivThree.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def maxSumDivThree(self, A):
+        dp = [0, 0, 0]
+        for a in A:
+            for i in dp[:]:
+                dp[(i + a) % 3] = max(dp[(i + a) % 3], i + a)
+        return dp[0]
